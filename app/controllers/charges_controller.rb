936577fe73cce9f4fb7359fd4af5b3b07d8705a6
Jen Haines <motochick67@gmail.com>
2014-11-26 20:09:30 -0800

    Stripe Integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,47 +1,39 @@
 class ChargesController < ApplicationController
 
-
-class Amount
-
-  def self.default
-    1500
+  def new
   end
-end
+
 
   def create
- 
-   # Creates a Stripe Customer object, for associating
-   # with the charge
-   customer = Stripe::Customer.create(
-     email: current_user.email,
-     card: params[:stripeToken]
-   )
- 
-   # Where the real magic happens
-   charge = Stripe::Charge.create(
-     customer: customer.id, # Note -- this is NOT the user_id in your app
-     amount: Amount.default,
-     description: "BigMoney Membership - #{current_user.email}",
-     currency: 'usd'
-   )
- 
-   flash[:success] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-   redirect_to root_path # or wherever
- 
- # Stripe will send back CardErrors, with friendly messages
- # when something goes wrong.
- # This `rescue block` catches and displays those errors.
- rescue Stripe::CardError => e
-   flash[:error] = e.message
-   redirect_to new_charge_path
+    @user = User.find_by :name => current_user.name
 
- end
-  def new
-    @stripe_btn_data = {
-      key: "#{ Rails.configuration.stripe[:publishable_key] }",
-      description: "BigMoney Membership - #{current_user.name}",
-      amount: Amount.default
-    }
+    @amount = 1500
+
+    customer = Stripe::Customer.create(
+    :email => 'example@stripe.com',
+    :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+    )
+    @user.role = "premium"
+    @user.save!
+    
+
+    redirect_to edit_user_registration_path
+    flash[:notice] = "Congratulations, you are a Premium Member!"
+
+
+    # redirect_to root_path # or wherever
+
+rescue Stripe::CardError => e
+    flash[:error] = e.message
+    
+
+
   end
-
 end
